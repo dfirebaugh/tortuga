5fe160c1b5283554760c33a98670ecd32f8af78b
Dustin Firebaugh <dafirebaugh@gmail.com>
2023-02-24 13:28:35 -0500

    removing unecessary pointer receivers from `component.Coordinate`

--- pkg/component/coordinate.go
+++ pkg/component/coordinate.go
@@ -12,7 +12,7 @@ type Coordinate struct {
 	Y float64
 }
 
-func (c *Coordinate) String() string {
+func (c Coordinate) String() string {
 	return fmt.Sprintf("%d, %d", int(c.X), int(c.Y))
 }
 
@@ -21,14 +21,14 @@ func (c *Coordinate) SetCoordinate(newCoord Coordinate) {
 	c.Y = newCoord.Y
 }
 
-func (c *Coordinate) GetDistance(other Coordinate) float64 {
+func (c Coordinate) GetDistance(other Coordinate) float64 {
 	a := geom.MakeVector(c.X, c.Y)
 	b := geom.MakeVector(other.X, other.Y)
 
 	return a.GetDistance(b)
 }
 
-func (c *Coordinate) GetDirection(other Coordinate) float64 {
+func (c Coordinate) GetDirection(other Coordinate) float64 {
 	a := geom.MakeVector(c.X, c.Y)
 	b := geom.MakeVector(other.X, other.Y)
 	// point := a.Add(b).ToPoint()
@@ -36,21 +36,21 @@ func (c *Coordinate) GetDirection(other Coordinate) float64 {
 	return a.GetDirection(b)
 }
 
-func (c *Coordinate) Add(other Coordinate) Coordinate {
+func (c Coordinate) Add(other Coordinate) Coordinate {
 	return Coordinate{
 		X: c.X + other.X,
 		Y: c.Y + other.Y,
 	}
 }
 
-func (c *Coordinate) Subtract(other Coordinate) Coordinate {
+func (c Coordinate) Subtract(other Coordinate) Coordinate {
 	return Coordinate{
 		X: c.X - other.X,
 		Y: c.Y - other.Y,
 	}
 }
 
-func (c *Coordinate) TranslateXY(offset Coordinate, pixelSize float64) (float64, float64) {
+func (c Coordinate) TranslateXY(offset Coordinate, pixelSize float64) (float64, float64) {
 	x := (c.X - offset.X) / pixelSize
 	y := (c.Y - offset.Y) / pixelSize
 	return x, y
